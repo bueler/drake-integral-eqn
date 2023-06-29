e126ce00d293be020c2a8af19c74978b087c6a87
Ed Bueler <elbueler@alaska.edu>
2023-06-29 12:23:54 -0800

    clearer and cleaner notation, and seemingly faster for some reason

--- fredholm.py
+++ fredholm.py
@@ -1,48 +1,51 @@
 from firedrake import *
-import numpy as np 
 
-# We consider the integral equation
-#
-# u(x) - 0.5\int_{0}^{1}(x+1)\exp{-xy}u(y)dy = f(x)   in [0, 1],
-#
-# where f(x) = \exp{-x} - 0.5 + 0.5\exp{-(x+1)}.  Here the kernel
-# is
-#    K(x,y) = -0.5 (x+1) exp(-xy)
-# and the equation is
-#    u(x) + int_0^1 K(x,y) u(y) dy = f(x)
-# The problem has an exact solution u(x) = exp(-x).
-
-def fredholm(N):
+def fredholm_numpy(N, K=None, f=None):
+    '''Solve the Fredholm second-kind integral equation
+       u(x) + int_0^1 K(x,y) u(y) dy = f(x)
+    for u(x) defined on [0,1], using P1 elements and the numpy
+    direct solver.'''
     mesh = UnitIntervalMesh(N)
     x = SpatialCoordinate(mesh)[0]
     V = FunctionSpace(mesh, 'CG', 1)
     v = TestFunction(V)
-    f = exp(-x) - 0.5 + 0.5 * exp(-(x+1))
-    b = assemble(f * v * dx).dat.data
+    b = assemble(f(x) * v * dx).dat.data
     # build dense A by columns
+    import numpy as np
     A = np.zeros((N+1,N+1))
     phij = Function(V)
     hx = 1.0 / N
     Y = Constant(0.0)
-    Kj = -0.5 * (x + 1) * exp(- x * Y)
     for j, yj in enumerate(mesh.coordinates.dat.data):
         phij.dat.data[:] = 0.0
         phij.dat.data[j] = 1.0
-        Y.assign(yj)
+        # note  c * K(x,Y) approximates  int_0^1 K(x,y) phi_j(y) dy
         if (j == 0) or (j == N):
             c = 0.5 * hx
         else:
             c = hx
-        # c * Kj approximates  int_0^1 K(x,y) phi_j(y) dy:
+        Y.assign(yj)
         A[:, j] = assemble(phij * v * dx +
-                           c * Kj * v * dx).dat.data
-    # solve A u = b
+                           c * K(x,Y) * v * dx).dat.data
     u = Function(V)
-    u.dat.data[:] = np.linalg.solve(A, b)
-    u_exact = Function(V).interpolate(exp(-x))
-    return errornorm(u_exact, u, 'L2')
+    u.dat.data[:] = np.linalg.solve(A, b) # solve A u = b
+    return V, x, u
+
+def fredholm_petsc(N, K=None, f=None):
+    raise NotImplementedError
 
 if __name__ == '__main__':
-    for N in [8,16,32,64,128,256]:
-        e = fredholm(N=N)
-        print('N=%3d: hx = %.6f,  error=%.3g' % (N, 1.0/N, e))
+    def K(x,y):
+        return - 0.5 * (x + 1) * exp(-x * y)
+
+    def f(x):
+        return exp(-x) - 0.5 + 0.5 * exp(-(x+1))
+
+    def uexact(x):
+        return exp(-x)
+
+    for N in [8,16,32,64,128,256,512,1024]:
+        V, x, u = fredholm_numpy(N=N, K=K, f=f)
+        uex = Function(V).interpolate(uexact(x))
+        e = errornorm(uex, u, 'L2')
+        print('N=%3d: error=%.3g' % (N, e))
