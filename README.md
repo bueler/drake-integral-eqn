6533128adaf6042cf362cf7d2c6768068816aefb
Ed Bueler <elbueler@alaska.edu>
2023-06-28 22:10:56 -0800

    a start ... slow assembly

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # drake-integral-eqn
 
-The [Firedrake]() finite element library is usually used to discretize the weak forms of partial differential equations (PDEs), but here we adapt it to integral equations.
+The [Firedrake](https://www.firedrakeproject.org) finite element library is usually used to discretize the weak forms of partial differential equations (PDEs), but here we want to demonstrate how to adapt it to integral equations.  This is not completely natural!
+
+For a start we consider a linear integral equation and merely create a numpy linear system $Ac=b$ and call `numpy.linalg.solve()` on it.
 
 ## example
 
-For example, a well-understood type of integral equation is
-
-$$u(x) + \int_0^1 K(x,y) u(y) dy = f(x)$$
-
-where we seek $u \in L^2(0,1)$ for given functions $K \in L^\infty(0,1)^2$ and $f\in L^2(0,1)$.
+A well-understood type of integral equation is
+  $$u(x) + \int_0^1 K(x,y) u(y) dy = f(x)$$
+where we seek $u \in L^2(0,1)$ for given functions $K \in C([0,1]^2)$ and $f\in L^2(0,1)$.  The weak form of the above equation is
+  $$\int_0^1 u(x) v(x) dx + \int_0^1 \int_0^1 K(x,y) u(y) v(x) dy dx = \int_0^1 f(x) v(x) dx$$
+We can assemble a linear system $Ac=b$ column-by-column if we expand $u$ in basis (hat) functions,
+  $$u(x) = \sum_{j=0}^{N} c_j \phi_j(x)$$
+Inserting this into the weak form and using $v(x)=\phi_i(x)$ we get an entry of the matrix $A$:
+  $$a_{ij} = \int_0^1 \phi_j(x) \phi_i(x) dx + \int_0^1 \left(\int_0^1 K(x,y) \phi_j(y) dy\right) \phi_i(x) dx$$
+Also
+  $$b_i = \int_0^1 f(x) \phi_i(x) dx$$
+FIXME continue
--- fredholm.py
+++ fredholm.py
@@ -1,74 +1,45 @@
-from dolfin import *
+from firedrake import *
 import numpy as np 
 
-# We show how integral equations can be solved with FEniCS
-# We consider an integral equation of the second type and want to find 
-# u: [0, 1] -> R, such that
+# We consider the integral equation
 #
 # u(x) - 0.5\int_{0}^{1}(x+1)\exp{-xy}u(y)dy = f(x)   in [0, 1],
 #
-# where f(x) = \exp{-x} - 0.5 + 0.5\exp{-(x+1)}. The problem has an exact
-# solution u(x) = exp(-x). The problem is taken from Linear Integral Equations
-# by Kress (p. 158)
+# where f(x) = \exp{-x} - 0.5 + 0.5\exp{-(x+1)}.  Here the kernel
+# is
+#    K(x,y) = -0.5 (x+1) exp(-xy)
+# and the equation is
+#    u(x) + int_0^1 K(x,y) u(y) dy = f(x)
+# The problem has an exact solution u(x) = exp(-x).
 
-f = Expression('exp(-x[0])-0.5+0.5*exp(-(x[0]+1))')
-
-def fredholm(N=32):
-    'Solve the problem on mesh with N elements.'
-    # Discretize the domain
+def fredholm(N):
     mesh = UnitIntervalMesh(N)
-
-    # Setup Vh
+    x = SpatialCoordinate(mesh)[0]
     V = FunctionSpace(mesh, 'CG', 1)
-    u = TrialFunction(V)
     v = TestFunction(V)
-
-    # The discretized problem becomes M*U - 0.5*M*C*U = b
-    # where M is the mass matrix, C is the matrix of the kernel, b is the load 
-    # vector and U has unknown expansion coefficients of the approximate solution
-    # uh
-
-    # Mass matrix
-    m_form = inner(u, v)*dx
-    M = assemble(m_form).array()
-
-    kernel = Expression('(t+1)*exp(-t*x[0])', t=0, degree=8)
-    x_ks = V.dofmap().tabulate_all_coordinates(mesh).reshape((-1, 1))[:, 0]
-
-    # Kernel matrix
-    C = np.zeros(M.shape)
-    # The row of C for x_k fixed can be made from the linear form
-    row_form = inner(kernel, v)*dx
-    for row, x_k in enumerate(x_ks):
-        kernel.t = x_k
-        row_values = assemble(row_form).array()
-        C[row, :] = row_values
-
-    # Load vector
-    L = inner(f, v)*dx
-    b = assemble(L).array()
-
-    A = M - 0.5*M.dot(C)
-    U = np.linalg.solve(A, b)
-
-    # Create uh
-    uh = Function(V)
-    uh.vector()[:] = U
-
-    # Compare with exact solution
-    u_exact = Expression('exp(-x[0])')
-
-    return errornorm(u_exact, uh, 'L2'), mesh.hmin()
-
-# -----------------------------------------------------------------------------
+    f = exp(-x) - 0.5 + 0.5 * exp(-(x+1))
+    b = assemble(f * v * dx).dat.data
+    # build dense A by columns
+    A = np.zeros((N+1,N+1))
+    phij = Function(V)
+    hx = 1.0 / N
+    for j, yj in enumerate(mesh.coordinates.dat.data):
+        phij.dat.data[:] = 0.0
+        phij.dat.data[j] = 1.0
+        K = -0.5 * (x + 1) * exp(- x * yj)  # how to substitute value for y in UFL for K(x,y)?
+        # Kj approximates  int_0^1 K(x,y) phi_j(y) dy:
+        if (j == 0) or (j == N):
+            Kj = Function(V).interpolate(0.5 * hx * K)
+        else:
+            Kj = Function(V).interpolate(hx * K)
+        A[:, j] = assemble(phij * v * dx + Kj * v * dx).dat.data
+    # solve A u = b
+    u = Function(V)
+    u.dat.data[:] = np.linalg.solve(A, b)
+    u_exact = Function(V).interpolate(exp(-x))
+    return errornorm(u_exact, u, 'L2')
 
 if __name__ == '__main__':
-    from math import log as ln
-
-    # Run the convergence test
-    for N in [16, 32, 64, 128, 256, 512, 1024]:
-        e, h = fredholm(N=N)
-        if N != 16:
-            rate = ln(e/e_)/ln(h/h_)
-            print 'N=%d error=%4g rate=%.2f' % (N, e, rate)
-        e_, h_ = e, h
+    for N in [2,4,8,16,32,64]:
+        e = fredholm(N=N)
+        print('N=%3d: hx = %.6f,  error=%.3g' % (N, 1.0/N, e))
